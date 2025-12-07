519da8ad6ebf1956104447101e66c2677b7813bc
Sandeep Nigam <sandeep.nigam83@gmail.com>
2025-12-07 21:37:53 +0530

    Remove timestamp from vite config, use pure content hash

--- vite.config.ts
+++ vite.config.ts
@@ -12,9 +12,9 @@ export default defineConfig(({ mode }) => {
         emptyOutDir: true,
         rollupOptions: {
           output: {
-            entryFileNames: `assets/[name]-[hash]-${Date.now()}.js`,
-            chunkFileNames: `assets/[name]-[hash]-${Date.now()}.js`,
-            assetFileNames: `assets/[name]-[hash]-${Date.now()}.[ext]`
+            entryFileNames: `assets/[name]-[hash].js`,
+            chunkFileNames: `assets/[name]-[hash].js`,
+            assetFileNames: `assets/[name]-[hash].[ext]`
           }
         }
       },
